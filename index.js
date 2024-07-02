326f6bcd7099f8aa3e3b072de0a4760f161cfd35
gavin <gavinmichori@gmail.com>
2024-07-02 13:29:10 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+// multiply
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+console.log(multiply);
+
+// random...
+let random =Math.floor(Math.random() * 10) + 1;
+console.log("random: " + random);
+
+// mod
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+console.log(mod);
+
+
+// max
+
+let max = Math.max(15, 20, 17, 4, 3, 16, 14)
+console.log("maximum: " + max);
+
